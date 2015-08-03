3253a77d296d8bfb831b676aad742d19907b019f
user <dvgrishechkin@yandex.ru>
2015-08-03 11:27:09 +0300

    1. Added: JUnit 4 support.

---
+++ app/src/test/java/ApplicationTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * JUnit 4 framework test.
+ */
+public class ApplicationTest {
+
+    @Test
+    public void test() {
+        assertTrue(true);
+    }
+}
