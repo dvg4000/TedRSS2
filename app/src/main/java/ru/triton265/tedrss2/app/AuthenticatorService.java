e4936d70ca973cff5a75963f3467f9ce8508b632
user <dvgrishechkin@yandex.ru>
2015-08-06 10:46:34 +0300

    1. Added: Stub authenticator

---
+++ app/src/main/java/ru/triton265/tedrss2/app/AuthenticatorService.java
@@ -0,0 +1,19 @@
+package ru.triton265.tedrss2.app;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class AuthenticatorService extends Service {
+    private StubAuthenticator mAuthenticator;
+
+    @Override
+    public void onCreate() {
+        mAuthenticator = new StubAuthenticator(this);
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return mAuthenticator.getIBinder();
+    }
+}
